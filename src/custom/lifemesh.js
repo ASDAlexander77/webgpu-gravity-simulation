9ef603b679817f1ba6fa47760cf622e6fd922714
AlexD <59564676+ODuzhar@users.noreply.github.com>
2022-06-04 00:03:24 +0100

    fixes fo life sim

--- src/custom/lifemesh.js
+++ src/custom/lifemesh.js
@@ -8,43 +8,31 @@ class LifeMesh extends ComputedMesh {
         const z = 0.0;
         const radius = 1;
 
-        let x = -radius;
-        let y = radius;
         const step = (2 * radius) / (width - 1);
 
         const instancesInitData = [];
-        for (let i = 0; i < this.instanceCount; i++) {
-            // position
-            instancesInitData.push(x);
-            instancesInitData.push(y);
-            instancesInitData.push(z);
-            //instancesInitData.push(radius * Math.random());
-            instancesInitData.push(0); // align, FYI: without align compute shader will not point to correct offset
+        for (let y = 0; y < width; y++)
+            for (let x = 0; x < width; x++) {
+                // position
+                instancesInitData.push(x * step - radius);
+                instancesInitData.push(y * step - radius);
+                instancesInitData.push(z);
+                //instancesInitData.push(radius * Math.random());
+                instancesInitData.push(0); // align, FYI: without align compute shader will not point to correct offset
 
-            // size, align, align, align
-            let size = step;            
+                // size, align, align, align
+                let size = step;            
 
-            if (Math.random() < 0.5)
-            {
-                size = 0;
-            }
-
-            instancesInitData.push(size);
-            instancesInitData.push(0);
-            instancesInitData.push(0); // align, FYI: without align compute shader will not point to correct offset
-            instancesInitData.push(0); // align, FYI: without align compute shader will not point to correct offset
-
-            x += step;
-            if (x >= radius)
-            {
-                x = -radius;
-                y -= step;
-                if (y <= -radius)
+                if (Math.random() < 0.5)
                 {
-                    y = radius;
+                    size = 0;
                 }
+
+                instancesInitData.push(size);
+                instancesInitData.push(0);
+                instancesInitData.push(0); // align, FYI: without align compute shader will not point to correct offset
+                instancesInitData.push(0); // align, FYI: without align compute shader will not point to correct offset
             }
-        }
 
         const instanceLayout = [
             // instance position
